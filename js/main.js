05195a206e518bd53bf560fc2497fe7d8c559a6e
Javier López <>
2022-03-02 19:18:23 +0100

    Landing

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.getElementById("myAudio").volume = 0.15;
+
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+var text = "Workout Tracker"
+ctx.textAlign = "center"
+ctx.font = "40px Signika";
+ctx.strokeStyle = "yellow"
+ctx.strokeText("Workout Tracker", 140, 80);
